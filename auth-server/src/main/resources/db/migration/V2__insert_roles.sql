53e13457effc9f28bd15ad1d48e47d38763c5cbc
Dev_Nicolas Geovane <devnicolasmateus@gmail.com>
2026-03-30 11:48:43 -0300

    c8 - Project finished

--- auth-server/src/main/resources/db/migration/V2__insert_roles.sql
+++ auth-server/src/main/resources/db/migration/V2__insert_roles.sql
@@ -1,3 +1,3 @@
-INSERT INTO `role` (`role`) VALUES ("ROLE_USER");
+INSERT INTO `role` (`role`) VALUES ('ROLE_USER');
 
-INSERT INTO `role` (`role`) VALUES ("ROLE_ADMIN");
+INSERT INTO `role` (`role`) VALUES ('ROLE_ADMIN');
